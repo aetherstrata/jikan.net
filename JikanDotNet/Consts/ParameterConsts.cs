7e316a7411986ccf291b04b7e980042cfd60a6f0
Davide Pierotti <d.pierotti@live.it>
2024-03-14 17:59:16 +0100

    Add parameter names to ParameterConsts.cs

--- JikanDotNet/Consts/ParameterConsts.cs
+++ JikanDotNet/Consts/ParameterConsts.cs
@@ -1,7 +1,7 @@
 namespace JikanDotNet.Consts
 {
     /// <summary>
-    /// Constants values related to limit of api.
+    /// Constant values related to API query parameters.
     /// </summary>
     public static class ParameterConsts
     {
@@ -9,5 +9,33 @@ namespace JikanDotNet.Consts
         /// Maximum page size for general requests
         /// </summary>
         public const int MaximumPageSize = 25;
+
+        public const string EndDate = "end_date";
+        public const string ExcludedGenres = "genres_exclude";
+        public const string Filter = "filter";
+        public const string Gender = "gender";
+        public const string Genres = "genres";
+        public const string Kids = "kids";
+        public const string Letter = "letter";
+        public const string Limit = "limit";
+        public const string MaxAge = "max_age";
+        public const string MaxScore = "max_score";
+        public const string MinAge = "min_age";
+        public const string MinScore = "min_score";
+        public const string OrderBy = "order_by";
+        public const string Page = "page";
+        public const string Preliminary = "preliminary";
+        public const string Producers = "producers";
+        public const string Query = "q";
+        public const string Rating = "rating";
+        public const string SafeForWork = "sfw";
+        public const string Score = "score";
+        public const string Sort = "sort";
+        public const string StartDate = "start_date";
+        public const string Spoiler = "spoiler";
+        public const string Spoilers = "spoilers";
+        public const string Status = "status";
+        public const string Type = "type";
+        public const string Unapproved = "unapproved";
     }
 }
